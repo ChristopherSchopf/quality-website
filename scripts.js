e91448e6a861b026af56d49ba908be9721715fd9
Christopher Schopf <christopherschopf@gmail.com>
2024-11-02 17:37:49 -0400

    Initial Upload
    Upload website.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const nav = document.querySelector('.hide');
+    nav.classList.toggle('show');
+}
+
+function hideSideBar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
